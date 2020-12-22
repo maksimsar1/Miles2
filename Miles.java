abad552348b7e3a06db9bbff7a3b2e2a83eff123
maksim <maksimsar1@gmail.com>
2020-12-22 20:51:44 +0400

    initial commit

---
+++ Miles.java
@@ -0,0 +1,7 @@
+public class Miles {
+    public static void main(String[] args) {
+        int price = 2000;
+        int charge = 20;
+        int bonus = price / charge;
+        System.out.println(bonus);
+    }
